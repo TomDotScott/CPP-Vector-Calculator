4f7dccfc57846458a525cee6fb25dc6414667910
Tom Scott <48756858+TomDotScott@users.noreply.github.com>
2020-02-04 12:35:57 +0000

    Expanded Vector.h

--- VectorCalculator/Vector.h
+++ VectorCalculator/Vector.h
@@ -5,12 +5,25 @@ public:
 	float getX() {
 		return x;
 	}
+
 	float getY() {
 		return y;
 	}
+
 	float getZ() {
 		return z;
 	}
+
+	Vector();
+
+	//for 2d vectors
+	Vector(float x, float y);
+
+	//for 3d vectors
+	Vector(float x, float y, float z);
+
+	float magnitude();
+
 private:
 	float x = 0;
 	float y = 0;
