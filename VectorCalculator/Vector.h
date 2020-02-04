5b6fa05b2dd477c0246be9b6111b8fedfe4cb565
Tom Scott <48756858+TomDotScott@users.noreply.github.com>
2020-02-04 12:32:19 +0000

    Initial Commit

---
+++ VectorCalculator/Vector.h
@@ -0,0 +1,20 @@
+#pragma once
+class Vector
+{
+public:
+	float getX() {
+		return x;
+	}
+	float getY() {
+		return y;
+	}
+	float getZ() {
+		return z;
+	}
+private:
+	float x = 0;
+	float y = 0;
+	float z = 0;
+
+};
+
